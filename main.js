d5013253ba9cd2bde20a2dddff6f637f48fc6ac0
asahi812-max <tpok.cca5@gmail.com>
2025-12-11 15:21:20 +0900

    update: ページの修正

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // main.js - ボタンで挨拶を表示するだけの簡単スクリプト
+
+
 document.addEventListener("DOMContentLoaded", function() {
   const btn = document.getElementById("helloBtn");
   const res = document.getElementById("helloResult");
